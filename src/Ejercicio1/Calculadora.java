8a43c158731fde122cdf77e74026a9927fb96caa
yeraf <yeraf@HIJO>
2026-04-02 11:27:09 +0200

    Añadida clase Calculadora.

--- src/Ejercicio1/Calculadora.java
+++ src/Ejercicio1/Calculadora.java
@@ -2,4 +2,15 @@ package Ejercicio1;
 
 public class Calculadora {
 
+	public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
 }
