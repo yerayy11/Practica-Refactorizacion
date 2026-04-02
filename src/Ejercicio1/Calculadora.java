cba96ca61a5990be63b33c052b9a09ff8c5c1b54
yeraf <yeraf@HIJO>
2026-04-02 11:31:57 +0200

    Añadida clase Calculadora.

--- src/Ejercicio1/Calculadora.java
+++ src/Ejercicio1/Calculadora.java
@@ -1,24 +1,5 @@
 package Ejercicio1;
 
-	import java.util.List;
+public class Calculadora {
 
-	public class Calculadora {
-
-	    public void p(List<Double> l, String n, double imp, boolean vip) {
-	        double t = 0;
-
-	        for (Double d : l) {
-	            t += d;
-	        }
-
-	        if (vip) {
-	            t = t - (t * 0.10);
-	            System.out.println("Cliente VIP: " + n);
-	            System.out.println("Total con descuento: " + t);
-	        } else {
-	            t = t + (t * imp);
-	            System.out.println("Cliente Normal: " + n);
-	            System.out.println("Total con impuestos: " + t);
-	        }
-	    }
-	}
+}
